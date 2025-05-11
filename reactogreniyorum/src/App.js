77f0c188f0c52fb9b607872321dd750f930fd9ef
alpsarikisla <50963484+alpsarikisla@users.noreply.github.com>
2025-05-11 10:09:09 +0300

    initial commit

---
+++ reactogreniyorum/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  const naber = "Napin?";
+  return (
+    <div className="App">
+      <h2>Selam</h2>
+      <div style={{backgroundColor:'red'}}>a</div>
+      <label>{10+10}</label><br/>
+      <label>{"Alp Sarıkışla".toUpperCase()}</label><br/>
+      <label>Murtaza = {naber}</label>
+    </div>
+  );
+}
+
+export default App;
